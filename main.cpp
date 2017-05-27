cb83e4ef0672331cdb879df6aa0fe8512a3ebd7d
林煜凱 <yukailin0911@gmail.com>
2017-05-27 16:31:29 +0800

    Fix bug and modify command prompt in main.cpp
    This commit fixes a bug in the second parameter in cin.getline, which
    should be BUF_SIZE-1 instead of BUF_SIZE. Also, for better UX, it
    modifies command prompt shown in the beginning of program execution.

--- main.cpp
+++ main.cpp
@@ -3,15 +3,19 @@
 
 using namespace std;
 
-#define BUF_SIZE 40
+const size_t BUF_SIZE = 101;
 
 int main() {
     Calculator myCal = Calculator();
     char expression[BUF_SIZE];
 
     while (1) {
-	cout << "Enter an arithmetic expression: \n";
-	cin.getline(expression, BUF_SIZE, '\n');
+	cout << "===== Press Ctrl+C to exit. =====\n";
+	cout << "Valid input characters: 0123456789()=-*/\n";
+	cout << "Enter an arithmetic expression within "
+	     << BUF_SIZE - 1 << " characters: \n";
+
+	cin.getline(expression, BUF_SIZE - 1, '\n');
 	myCal.calculate(expression);
     }
 
